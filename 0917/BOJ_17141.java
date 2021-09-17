6e19e8c16c4bfdfc47fdc2d8cb620f5f241a141d
ko509 <62974220+ko509@users.noreply.github.com>
2021-09-18 02:21:02 +0900

    Update BOJ_17141.java

--- 0917/BOJ_17141.java
+++ 0917/BOJ_17141.java
@@ -16,7 +16,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.Inet4Address;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
