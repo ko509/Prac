d5d6984ec8666fc9e158d648262923eeec8b67aa
ko509 <62974220+ko509@users.noreply.github.com>
2022-02-25 21:40:08 +0900

    최소 힙

---
+++ BOJ/BOJ_1927.java
@@ -0,0 +1,39 @@
+package _202202_sk_jina;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+
+// 시간 : 304ms
+// 메모리 : 25904KB
+public class BOJ_1927 {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		PriorityQueue<Long> que = new PriorityQueue<>();
+		StringBuilder sb = new StringBuilder();
+		int N = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			long n = Long.parseLong(br.readLine());
+			if(n==0) {
+				if(que.isEmpty()) {
+					sb.append(0);
+					sb.append("\n");
+				}else {
+					sb.append(que.poll());
+					sb.append("\n");
+				}
+			}else {
+				que.offer(n);
+				System.out.println(Arrays.toString(que.toArray()));
+			}
+			System.out.println(sb);
+		}
+		System.out.println(sb);
+	}
+
+}
