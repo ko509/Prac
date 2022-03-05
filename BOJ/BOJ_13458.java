dc67babd54c98926a3e00b5deef4795260e28b9f
ko509 <62974220+ko509@users.noreply.github.com>
2022-03-05 23:39:59 +0900

    시험 감독

--- BOJ/BOJ_13458.java
+++ BOJ/BOJ_13458.java
@@ -3,6 +3,8 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
+// 시간 : 476ms
+// 메모리 : 145252
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
